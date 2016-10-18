0d1cf11b1fdf36cd398c98b7038574cf59519cb4
Hasit Mistry <hasitnm@gmail.com>
2016-10-17 18:02:10 -0700

    Add default user keys on creation

--- db.go
+++ db.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"time"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/boltdb/bolt"
@@ -17,17 +18,54 @@ func (i *impl) initDB() {
 	gopath := os.Getenv("GOPATH")
 	ghjsDBPath := path.Join(gopath, "src", "github.com", "ghjobs-subscribe", "ghjobs-subscribe", "subs.db")
 	var err error
-	i.DB, err = bolt.Open(ghjsDBPath, 0600, nil)
+	i.DB, err = bolt.Open(ghjsDBPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
 	if err != nil {
 		logrus.Fatal(err)
 	}
 }
 
+// frequence: weekly (1), fortnightly (2), monthly (3)
 func (i *impl) createUserBucket(email string) bool {
 	err := i.DB.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucket([]byte(email))
+		b, err := tx.CreateBucket([]byte(email))
 		if err != nil {
 			return fmt.Errorf("create bucket: %v", err)
+		}
+		err = b.Put([]byte("email"), []byte(email))
+		if err != nil {
+			return err
+		}
+		err = b.Put([]byte("firstname"), []byte(""))
+		if err != nil {
+			return err
+		}
+		err = b.Put([]byte("lastname"), []byte(""))
+		if err != nil {
+			return err
+		}
+		err = b.Put([]byte("ghjobstag"), []byte(""))
+		if err != nil {
+			return err
+		}
+		err = b.Put([]byte("ghjobslocation"), []byte(""))
+		if err != nil {
+			return err
+		}
+		err = b.Put([]byte("frequence"), []byte("1"))
+		if err != nil {
+			return err
+		}
+		err = b.Put([]byte("lastsent"), []byte(""))
+		if err != nil {
+			return err
+		}
+		err = b.Put([]byte("nextsend"), []byte(""))
+		if err != nil {
+			return err
+		}
+		err = b.Put([]byte("createdon"), []byte(time.Now().String()))
+		if err != nil {
+			return err
 		}
 		return nil
 	})
