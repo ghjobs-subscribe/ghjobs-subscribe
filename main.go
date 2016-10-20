3da149e26bafb2e20e56021e8c959582cd3d490a
Hasit Mistry <hasitnm@gmail.com>
2016-10-20 11:52:27 -0700

    Remove fvbock/endless

--- main.go
+++ main.go
@@ -1,24 +1,11 @@
 package main
 
-import (
-	"log"
-	"os"
-
-	"github.com/fvbock/endless"
-	"github.com/gin-gonic/gin"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
 	r.POST("/subscribe", subscribeHandler)
 	// r.POST("/manage", manageHander)
 	r.POST("/unsubscribe", unsubscribeHandler)
-
-	err := endless.ListenAndServe(":8080", r)
-	if err != nil {
-		log.Println(err)
-	}
-	log.Println("ghjobs-subscribe server stopped on 8080")
-
-	os.Exit(0)
+	r.Run(":8080")
 }
