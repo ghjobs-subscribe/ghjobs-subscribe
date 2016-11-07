1665878d799472943ca6de6925c3e5153e8a4244
Hasit Mistry <hasitnm@gmail.com>
2016-11-07 11:33:37 -0800

    Add stats to check reponse time and status, etc.

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/thoas/stats"
+)
+
+// Stats provides response time, status code count, etc.
+var Stats = stats.New()
 
 func main() {
 	r := gin.Default()
+
+	r.Use(func() gin.HandlerFunc {
+		return func(c *gin.Context) {
+			beginning, recorder := Stats.Begin(c.Writer)
+			c.Next()
+			Stats.End(beginning, recorder)
+		}
+	}())
 
 	r.LoadHTMLGlob("templates/*")
 
@@ -13,5 +29,8 @@ func main() {
 	r.POST("manage/update", manageUpdateHandler)
 	r.POST("/unsubscribe", unsubscribeHandler)
 	r.GET("/unsubscribe/verify", unsubscribeVerifyHandler)
+	r.GET("/stats", func(c *gin.Context) {
+		c.JSON(http.StatusOK, Stats.Data())
+	})
 	r.Run(":8080")
 }
